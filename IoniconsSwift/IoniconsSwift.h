8526badbada24325eb64b65f6b064d5185c22423
Josh Baker <joshbaker77@gmail.com>
2015-06-09 10:32:23 -0700

    initial commit

---
+++ IoniconsSwift/IoniconsSwift.h
@@ -0,0 +1,19 @@
+//
+//  IoniconsSwift.h
+//  IoniconsSwift
+//
+//  Created by Josh Baker on 6/9/15.
+//  Copyright (c) 2015 ONcast, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IoniconsSwift.
+FOUNDATION_EXPORT double IoniconsSwiftVersionNumber;
+
+//! Project version string for IoniconsSwift.
+FOUNDATION_EXPORT const unsigned char IoniconsSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IoniconsSwift/PublicHeader.h>
+
+
